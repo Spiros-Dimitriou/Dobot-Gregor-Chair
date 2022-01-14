011a00ddd33cbe9e95c734a3088752d300277176
spiros-dimitriou <spiros.dimitriou776@gmail.com>
2022-01-14 10:57:25 +0200

    Updated Worker task 1

--- client/wt1.sh
+++ client/wt1.sh
@@ -1,4 +1,16 @@
+### Worker Task 1 ###
+
+echo "Worker Task 1 begins"
+echo ""
+
+echo "Request permission to use W1"
+
+sleep 1
+
+echo "W1 request granted"
+
 # grab the leg base and place it in the w1 gripper
+echo "Moving the leg base to the W1 gripper"
 curl --location --request POST "${R1_IP}:${R1_PORT}/placePart" \
 --header 'Content-Type: application/json' \
 --data-raw '{
@@ -11,13 +23,16 @@ curl --location --request POST "${R1_IP}:${R1_PORT}/placePart" \
     "z2": "30",
     "r2": "42"
 }'
+echo ""
 
 # todo: make w1 gripper grip
+echo "Have W1 grip the leg base"
 
 for i in 1 2
 do
 	# grab a leg and place it into the leg base
-		curl --location --request POST "${R1_IP}:${R1_PORT}/placePart" \
+	echo "Placing leg No $i"
+	curl --location --request POST "${R1_IP}:${R1_PORT}/placePart" \
 	--header 'Content-Type: application/json' \
 	--data-raw '{
 		"x1": "255",
@@ -29,6 +44,12 @@ do
 		"z2": "30",
 		"r2": "42"
 	}'
-	
-	# todo: make w1 gripper turn to be ready for next leg
+	echo ""
 done
+
+# todo: make w1 gripper turn to be ready for next leg
+echo "Have W1 turn its gripper"
+
+sleep 1
+
+echo "Leg base and legs liaison done"
