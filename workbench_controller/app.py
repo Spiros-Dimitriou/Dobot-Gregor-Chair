d04eb7f5b3e3d151cc51ae43a594478a25f16db9
Harry Touloupas <harry.touloupas@yahooinc.com>
2022-03-22 12:07:49 +0200

    Dockerize workbench controller

--- workbench_controller/app.py
+++ workbench_controller/app.py
@@ -1,6 +1,10 @@
+import logging
+
 from flask import Flask
 from flask import request
-import composites
+
+
+logging.basicConfig(format='%(process)d-%(levelname)s-%(message)s', level=logging.DEBUG)
 
 app = Flask(__name__)
 fixture_status = False
@@ -11,28 +15,31 @@ def hello_world():
     return "<h1>Hello, World!!</h1>"
 
 
-@app.route("/fix")
+@app.route("/fix", methods=["POST"])
 def fix():
     global fixture_status
     fixture_status = True
-    return "Fixture fixed"
+    return 200
 
 
 @app.route("/unfix")
 def unfix():
     global fixture_status
     fixture_status = False
-    return "Fixture unfixed"
+    return 200
 
 
 @app.route("/rotateFixture", methods=['POST'])
 def rotateFixture():
     body = request.get_json(force=True)
-    pos = body["pos"]
-    deg = body["deg"]
-    return f"Rotated fixture {pos} by {deg} degrees"
+    pos, deg = body["pos"], body["deg"]
+    return 200
 
 
 @app.route("/rotate")
 def rotate():
-    return "Workbench rotated"
+    return 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
