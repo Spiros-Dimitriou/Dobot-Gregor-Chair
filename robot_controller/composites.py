e91dfd417940211f734e8b794443cc4fc4e42081
Spiros-Dimitriou <55939695+Spiros-Dimitriou@users.noreply.github.com>
2022-03-28 03:28:10 +0300

    Added moveAndScrew composite uS

--- robot_controller/composites.py
+++ robot_controller/composites.py
@@ -4,3 +4,8 @@ def placePart(primitives, x1, y1, z1, r1, x2, y2, z2, r2):
     primitives.grip()
     primitives.move(x2, y2, z2, r2)
     primitives.ungrip()
+
+
+def moveAndScrew(primitives, x, y, z, r, deg):
+    primitives.move(x, y, z, r)
+    screw(deg)
