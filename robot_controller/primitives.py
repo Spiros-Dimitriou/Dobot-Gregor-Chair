e5873591f95496e4dfc192641d6d8f613f4d0b81
Spiros-Dimitriou <55939695+Spiros-Dimitriou@users.noreply.github.com>
2022-10-06 16:26:11 +0300

    Update primitives.py
    Make primitive commands wait to progress (block)

--- robot_controller/primitives.py
+++ robot_controller/primitives.py
@@ -18,24 +18,39 @@ class RobotController:
         self.doBot.wait(500)
         self.doBot.set_end_effector_gripper(False, False)
         self.doBot.set_point_to_point_command(1, 207, -150, 60, 0)
+        self.block()
 
     def move(self, x, y, z, r):
         self.doBot.set_point_to_point_command(1, x, y, z, r)
+        self.block()
 
     def grip(self):
         self.doBot.set_end_effector_gripper(True, True)
         self.doBot.wait(300)
+        self.block()
 
     def ungrip(self):
         self.doBot.set_end_effector_gripper(True, False)
         self.doBot.wait(300)
         self.doBot.set_end_effector_gripper(False, False)
+        self.block()
 
     def wait(self, ms):
         self.doBot.wait(ms)
+        self.block()
 
     def getPose(self):
         return self.doBot.get_pose()[0:4]
 
     def clearAlarms(self):
         self.doBot.clear_alarms_state()
+
+    def block(self):
+        self.doBot.wait(0)
+
+        queue_index = self.doBot.get_current_queue_index()
+        while True:
+            if self.doBot.get_current_queue_index() > queue_index:
+                break
+
+            sleep(0.5)
